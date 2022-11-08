e8a002d7c52a4b9d6f89c9146266241f23956470
ARK <ark@Oblivion.local>
2022-11-08 21:40:36 +0530

    0.2

---
+++ Central_Server.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+import pickle
+
+class Central:
+    def __init__(self, port:int):
+        self.Ledger = []
+        self.Socket = self.Create_Socket(port)
+
+    def Create_Socket(self,port_no):
+        ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        ServerSocket.bind((socket.gethostname(), port_no))
+        return ServerSocket
+    
+    def Search_Peer(self, Item):
+        peer:socket.socket
+        seller_list=[]
+        for peer,addr in self.Ledger:
+            peer.send(Item.encode())
+            if peer.recv(1024).decode() == "POSITIVE":
+                seller_list.append(addr)
+        
+        if len(seller_list) == 0: return None
+        
+        sl_bin = pickle.dumps(seller_list)  # CONVERTS PYTHON OBJ TO BYTES
+        return sl_bin
+
+    def Communicate(self, client_socket:socket.socket):
+        while True:
+            message = client_socket.recv(1024).decode().split()
+            if message[0] == "QUERY":
+                sl_bin=self.Search_Peer(message[1])
+                if sl_bin==None:
+                    client_socket.send("Item Not Found".encode())
+                else:
+                    client_socket.send(sl_bin)
+                    chosen_seller:socket.socket
+                    chosen_seller = int(client_socket.recv().decode())
+                    chosen_seller = self.Ledger[chosen_seller][0]
+                    chosen_seller.send("Trade".encode())
+
+    def Awaken(self):
+        self.Socket.listen(10)
+        while True:
+            # accept connections from outside
+            (clientsocket, address) = self.Socket.accept()
+            self.Ledger.append((clientsocket,address))
+            # 
+            threading.Thread(target = self.Communicate,args = (clientsocket)).start()
+
