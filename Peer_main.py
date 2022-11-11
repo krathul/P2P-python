a26aec018b368871dbaf2e847d3321d91f525976
ARK <ark@Oblivion.local>
2022-11-11 08:52:39 +0530

    0.51

--- Peer_main.py
+++ Peer_main.py
@@ -2,12 +2,13 @@ import sys, peers, socket, random
 
 if __name__ == "__main__":
     item_list=[]
-    with open('Items.csv') as f:
-        item_list=f.readline().split(',')
+    with open('items_list.txt') as f:
+        item_list = f.readlines()
+        item_list = list(map(str.strip, item_list))
     
     peer = peers.Peer(socket.gethostbyname('localhost'), *sys.argv[1:])
     peer.connect_central(socket.gethostbyname('localhost'), 7890)
-    peer.seller_inventory = random.choices(item_list, k=4)
+    peer.seller_inventory = random.choices(item_list, k=8)
     print(peer.seller_inventory)
 
     ch = int(input("What would you like to do?\n1.Buy Item\n2.Add Item to Sell\nEnter : "))
